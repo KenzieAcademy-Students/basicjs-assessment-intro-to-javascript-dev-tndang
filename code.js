738cc8a9747b5ed749d3492d2181a23a3548434a
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-03-28 18:09:22 -0400

    Finally got the while loop to end and close the prompt.

--- code.js
+++ code.js
@@ -4,14 +4,11 @@
 // You can access the story by using the pages array.
 // You can find the pages array in story.js
 // The first page is at index 0.
-console.log(pages[0]);
 
 let endingPages = [4, 9, 13, 17, 19, 20];
 let currentPage = 0;
 
-// Your Code Here.
-currentPage = prompt(pages[0]);
-
+// Your Code Here.1
 let endingCheck = function(pageNum) {
     for (let i = 0; i <= endingPages.length; i++) {
         return pageNum === endingPages[i];
@@ -19,22 +16,36 @@ let endingCheck = function(pageNum) {
 }
 
 let printStory = function () {
-    let theStory = `<p>${pages[currentPage]}</p>`;
-    return theStory;
+    let resultString = ``;
+    if (currentPage === 0) {
+        resultString = `<p>${pages[currentPage]}</p>`;
+        return resultString;
+    } 
+    else if (endingCheck() === true) {
+            resultString += `<h2>The End</h2>`
+    } else {
+        resultString += `<p>You turned to page ${currentPage}.</p>`;
+        resultString += `<p>${pages[currentPage]}</p>`;
+    }
+    return resultString;
 }
 
 while (currentPage !== null) {
-    let pageCheck = endingCheck(currentPage);
+    currentPage = parseInt(currentPage);
     console.log(pages[currentPage]);
-    if (currentPage !== null && pageCheck !== true) {
+
+    let finalStory = printStory();
+    let endCheck = endingCheck(currentPage);
+
+    if (currentPage !== null && endCheck !== true) {
         currentPage = prompt(pages[currentPage]);
-        document.write(printStory());
-    } 
-    else if (currentPage === null && pageCheck !== false) {
-        document.write = "The End";
-    }
-    else {
+        document.write(finalStory);
+    } else if (endCheck === true) {
         currentPage = null;
+        document.write(finalStory);
+    } else {
+        document.write(finalStory);
+        break;
     }
 }
 
