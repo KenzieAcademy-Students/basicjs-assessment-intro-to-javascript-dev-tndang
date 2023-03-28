fea042bf758fcb6139469dd5377a905f2aaae354
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-03-28 22:09:50 +0000

    deploy: 738cc8a9747b5ed749d3492d2181a23a3548434a

--- code.js
+++ code.js
@@ -4,21 +4,49 @@
 // You can access the story by using the pages array.
 // You can find the pages array in story.js
 // The first page is at index 0.
-console.log(pages[0]);
 
 let endingPages = [4, 9, 13, 17, 19, 20];
 let currentPage = 0;
 
-// Your Code Here.
-
-let theEnd = function() {
+// Your Code Here.1
+let endingCheck = function(pageNum) {
     for (let i = 0; i <= endingPages.length; i++) {
-        if (currentPage === endingPages[i]) {
-            return true;
-        } else {
-            return false;
-        }
+        return pageNum === endingPages[i];
     }
 }
 
-console.log(theEnd());
+let printStory = function () {
+    let resultString = ``;
+    if (currentPage === 0) {
+        resultString = `<p>${pages[currentPage]}</p>`;
+        return resultString;
+    } 
+    else if (endingCheck() === true) {
+            resultString += `<h2>The End</h2>`
+    } else {
+        resultString += `<p>You turned to page ${currentPage}.</p>`;
+        resultString += `<p>${pages[currentPage]}</p>`;
+    }
+    return resultString;
+}
+
+while (currentPage !== null) {
+    currentPage = parseInt(currentPage);
+    console.log(pages[currentPage]);
+
+    let finalStory = printStory();
+    let endCheck = endingCheck(currentPage);
+
+    if (currentPage !== null && endCheck !== true) {
+        currentPage = prompt(pages[currentPage]);
+        document.write(finalStory);
+    } else if (endCheck === true) {
+        currentPage = null;
+        document.write(finalStory);
+    } else {
+        document.write(finalStory);
+        break;
+    }
+}
+
+
